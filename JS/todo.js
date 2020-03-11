77dce700658a94b1afdba65f13382f1e530859b4
sankareswari.m <sankareswari.m@zohocorp.com>
2020-03-11 21:00:14 +0530

    ok

--- JS/todo.js
+++ JS/todo.js
@@ -1,26 +1,15 @@
-// var completed=document.querySelector(".todo-item");
-//      completed.addEventListener("click",function(complete){
-//      if(complete.target.tagName==="DIV"){
-//          complete.target.classList.toggle("checked");
-//         //  complete.target.classList.toggle("completed")
-//      }
-//  },false);
-
 //add newtodo
 var newTodo=function(id,todoName) {
     this.id=id;
     this.todoName=todoName;
 };
 var todoItems=[];
+
 function addNewTodo(ele) {
-    //create the new id
     var Id=(todoItems.length == 0)? 0:todoItems[todoItems.length-1].id+1;
-    //get the value
     var todoName=document.getElementById("todo-inputbox").value;
-    //create the newitem
     var newItem=new newTodo(Id,todoName);
     console.log(newItem);
-    //push
     todoItems.push(newItem);
 
     var container=document.querySelector(".todo-container");
@@ -29,7 +18,7 @@ function addNewTodo(ele) {
                      '<div class="todo-edit"><i class="far fa-edit edit-icon" style="font-size:28px;" id="edit"></i></i></div>'+
                      '<div class="todo-favourite"><i class="fa fa-star-o favourite-icon" style="font-size:34px" id="favourite"></i></div>'+
                      '<div class="todo-remove"><i class="fa fa-trash-o remove-icon" style="font-size:34px" id="delete"></i></div></div>';
-    //replace
+
     var newHtml=HTMLString.replace("%id%",Id).replace("%todoName%",todoName);
 
     container.insertAdjacentHTML('beforeend',newHtml);         
@@ -54,10 +43,25 @@ removed.addEventListener("click",function(remove){
 var completed=document.querySelector(".todo-container");
 completed.addEventListener("click",function(complete){
     if(complete.target.id==="complete"){
-        var a=complete.target.id;    
-        console.log(a);
-        //a.classList.add("checked");        
+        var completeId=complete.target.parentNode;    
+        completeId.classList.toggle("checked"); 
+        completeId.classList.toggle("completed")       
     }
+});
+
+//favourite of the todo
+var favourite=document.querySelector(".todo-container");
+favourite.addEventListener("click",function(favour){
+    if(favour.target.id==="favourite"){
+        // var itemid,splitid,type;
+        //     itemid=remove.target.parentNode.parentNode.id;
+        //     splitid=itemid.split("-");
+        //     type=splitid[0];
+        //     ID=parseInt(splitid[1]);
+        var starIcon=favour.target.classList;
+        console.log(starIcon);
+        starIcon.classList.toggle("colorchange");
+    }   
 });
 
 //when the enter button was clicked 
@@ -65,4 +69,4 @@ document.addEventListener("keypress",function(event){
     if(event.keycode===13 || event.which===13){
         addNewTodo();
     }
-});
+}); 

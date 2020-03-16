cb3205c79933eb516907aedc03e801b2ff574045
sankareswari.m <sankareswari.m@zohocorp.com>
2020-03-16 16:45:04 +0530

    ok

--- JS/todo.js
+++ JS/todo.js
@@ -9,66 +9,59 @@ function addNewTodo(ele) {
     var Id=(todoItems.length == 0)? 0:todoItems[todoItems.length-1].id+1;
     var todoName=document.getElementById("todo-inputbox").value;
     var newItem=new newTodo(Id,todoName);
-    console.log(newItem);
     todoItems.push(newItem);
 
     var container=document.querySelector(".todo-container");
-    var HTMLString='<div class="todo-item" id="item-%id%"><i class="fa fa-circle-thin circle-icon" id="complete" style="font-size:34px"></i>'+
-                     '<div class="todo-item-input">%todoName%</div>'+
-                     '<div class="todo-edit"><i class="far fa-edit edit-icon" style="font-size:28px;" id="edit"></i></i></div>'+
+    var HTMLString=  '<div class="todo-item" id="item-%id%">'+
+                     '<div class="todo-check"><i class="fa fa-circle-thin circle-icon" id="complete" style="font-size:34px"></i></div>'+
+                     '<input class="todo-item-input" value="%todoName%"></input>'+
                      '<div class="todo-favourite"><i class="fa fa-star-o favourite-icon" style="font-size:34px" id="favourite"></i></div>'+
                      '<div class="todo-remove"><i class="fa fa-trash-o remove-icon" style="font-size:34px" id="delete"></i></div></div>';
 
     var newHtml=HTMLString.replace("%id%",Id).replace("%todoName%",todoName);
-
+    if(todoName==''){
+       newHtml='';
+    }
     container.insertAdjacentHTML('beforeend',newHtml);   
-
-    localStorage.setItem('todos',JSON.stringify(todoData));      
-    
+    //localStorage.setItem('todos',JSON.stringify(todoData));      
 }
 
 //delete the todo
 var removed=document.querySelector(".todo-container");
 removed.addEventListener("click",function(remove){
     if(remove.target.id=="delete"){
-            var itemid,splitid,type;
-            itemid=remove.target.parentNode.parentNode.id;
-            splitid=itemid.split("-");
-            type=splitid[0];
-            ID=parseInt(splitid[1]);
             var removeEle=remove.target.parentNode.parentNode;
-            removeEle.parentNode.removeChild(removeEle);            
+            removeEle.parentNode.removeChild(removeEle);   
     }
-    localStorage.setItem('todos',JSON.stringify(todoData)); 
 });
 
 //complete the todo
 var completed=document.querySelector(".todo-container");
 completed.addEventListener("click",function(complete){
     if(complete.target.id==="complete"){
-        var completeId=complete.target.parentNode;    
-        completeId.classList.toggle("checked"); 
-        completeId.classList.toggle("completed")       
+        var lineThroughEle=document.querySelector(".todo-item-input");
+        lineThroughEle.classList.toggle("checked");
+        var checkedElement=document.querySelector("#complete");
+        console.log(checkedElement);
+        var a=checkedElement.classList.toggle("completed") ;      
+        console.log(a);
     }
-    localStorage.setItem('todos',JSON.stringify(todoData)); 
+});
+
+//favourite of the task
+var favouriteTodo=document.querySelector(".todo-container");
+favouriteTodo.addEventListener("click",function(favour){
+    if(favour.target.id==="favourite"){
+        var favouriteId=favour.target.parentNode.parentNode; 
+        favouriteId.classList.toggle("favourite");
+    }
 });
 
 //when the enter button was clicked 
 document.addEventListener("keypress",function(event){
     if(event.keycode===13 || event.which===13){
         addNewTodo();
+        document.querySelector("#todo-inputbox").value='';
+        localStorage.setItem('todos',JSON.stringify(todoData));
     }
 }); 
-
-//edit the todo
-
-var edited=document.querySelector(".todo-container");
-edited.addEventListener("click",function(editing){
-    if(editing.target.id==="edit"){
-        var listItem=editing.target.parentNode;
-        console.log(listItem);
-        
-    }   
-});
-
-
